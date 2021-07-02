5018ed4c0948c33afc01b287d9f123afd13d7885
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-02 18:01:14 -0300

    build-fix: Make storybook work with tailwindcss

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,20 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\,css&/,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: [require("tailwindcss"), require("autoprefixer")],
+          },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
 };
