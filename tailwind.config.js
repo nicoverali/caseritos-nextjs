264fe4dc7075cfb3b287e7124b07a0280f2085d8
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-03 14:30:33 -0300

    feat: Add theme fonts and colors
    This includes loading fonts in both _document.js file and
    preview-head.html (for Storybook).
    
    Display fonts will only be added  to the homepage.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { colors } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: [
@@ -6,7 +8,27 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: {
+          light: "#333",
+          DEFAULT: "#222",
+          dark: "#111",
+          pure: "#000",
+        },
+        primary: {
+          light: colors.red[300],
+          DEFAULT: colors.red[400],
+          dark: colors.red[500],
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", "ui-sans-serif", "system-ui"],
+      display: ["Playfair Display", "Oswald"],
+      handwrite: ["Homemade Apple"],
+      body: ["Poppins", "Open Sans"],
+    },
   },
   variants: {
     extend: {},
