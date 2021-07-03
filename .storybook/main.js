15207ab398ccd91dabccb090e3c00fb75cf125f1
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-03 14:33:22 -0300

    build: Rename stories files
    The intended file structure is a folder per component containing:
     - index.tsx : the main file of the component
     - stories.tsx : component stories
     - test.tsx: unit test for the component
     - * : any other file related to the component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
-  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/**/stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   webpackFinal: async (config) => {
+    config.resolve.modules.push("../src/");
     config.module.rules.push({
       test: /\,css&/,
       use: [
