6d20d73135b745f2d61465fc0f7c7a05529a2da0
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-04 13:33:19 -0300

    refactor: Split TextInput and add classes prop
    Separate Label and ErrorMessage into their own component file.
    Add a classes prop to customize deeper elements in the component.

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,10 +1,17 @@
 import clsx from "clsx";
 import React from "react";
-import ControlledTextInput, {
-  ControlledTextInputProps,
-} from "./ControlledTextInput";
+import ControlledTextInput from "./ControlledTextInput";
+import { ControlledTextInputProps } from "./ControlledTextInput";
+import ErrorMessage from "./ErrorMessage";
+import Label from "./Label";
 
 interface TextInputProps extends ControlledTextInputProps {
+  classes: {
+    container?: string;
+    label?: string;
+    input?: string;
+    error?: string;
+  };
   label: string;
   hideLabel?: boolean;
   size?: "sm" | "md" | "lg";
@@ -21,22 +28,19 @@ const sizes = {
 function TextInput(props: TextInputProps) {
   const id = props.id || `input-${props.name}`;
   const size = sizes[props.size || "md"];
+  const classes = props.classes || {};
 
   return (
-    <div className={props.className}>
-      <label
-        className={clsx("text-sm font-medium block mb-1", {
-          hidden: props.hideLabel,
-        })}
-        htmlFor={id}
-      >
+    <div className={clsx(props.className, classes.container, props.width)}>
+      <Label forId={id} hidden={props.hideLabel} className={classes.label}>
         {props.label}
-      </label>
+      </Label>
 
       <ControlledTextInput
         {...props}
         id={id}
         className={clsx(
+          classes.input,
           props.width,
           size,
           "px-4 border border-gray-200 outline-none focus:border-black",
@@ -44,9 +48,7 @@ function TextInput(props: TextInputProps) {
         )}
       />
 
-      <p className={clsx("text-xs mt-1 text-error", { hidden: !props.error })}>
-        {props.error}
-      </p>
+      <ErrorMessage error={props.error} className={classes.error} />
     </div>
   );
 }
