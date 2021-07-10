dc890cfbd5016376e2a197b10b63a1a38230f4d4
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-10 11:24:53 -0300

    refactor: ProductCardContainer is no longer an Action
    Parent components can wrap the container with an Action

--- src/components/ProductCard/ProductCardContainer.tsx
+++ src/components/ProductCard/ProductCardContainer.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from "react";
-import Action from "components/Action";
 import clsx from "clsx";
 
 interface ContainerProps {
@@ -9,13 +8,13 @@ interface ContainerProps {
 
 export default function ProductCardContaine(props: ContainerProps) {
   return (
-    <Action
+    <div
       className={clsx(
         "flex flex-col ring-black outline-none rounded-none focus:ring hover:ring group transition-all duration-200 ease-bounce-in",
         props.classNames
       )}
     >
       {props.children}
-    </Action>
+    </div>
   );
 }
