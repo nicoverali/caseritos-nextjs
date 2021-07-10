0b18fcf3c4137aaf961b767be5a4c3ec85db667c
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-10 10:26:14 -0300

    fix: ClientForm didn't allow a className prop

--- src/components/ClientForm/index.tsx
+++ src/components/ClientForm/index.tsx
@@ -1,14 +1,16 @@
+import clsx from "clsx";
 import React, { HTMLProps, ReactNode } from "react";
 
 interface ClientFormProps {
   title: string;
   children: ReactNode;
+  className?: string;
   form?: HTMLProps<HTMLFormElement>;
 }
 
 function ClientForm(props: ClientFormProps) {
   return (
-    <div className="max-w-xl">
+    <div className={clsx("max-w-xl", props.className)}>
       <h1 className="text-6xl font-medium text-center">{props.title}</h1>
       <form
         className="flex flex-col mt-4 p-12 border-2 border-black"
