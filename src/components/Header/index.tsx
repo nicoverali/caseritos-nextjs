4cb5139f5164e6bce51a619deec1a42510a82593
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-10 10:44:41 -0300

    fix: Some Header component fixes
    - Change container element from div to header
    - Add z-50 property to display above everything
    - Receive a className prop
    - Change default unlifted background to white

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,23 +8,28 @@ interface HeaderProps {
   bgUnlift?: string;
   bgLift?: string;
   lift?: boolean;
+  className?: string;
   children?: ReactNode;
 }
 
 function Header(props: HeaderProps) {
-  const { bgUnlift = "bg-transparent", bgLift = "bg-white", bg } = props;
+  const { bgUnlift = "bg-white", bgLift = "bg-white", bg } = props;
   const { lift = false } = props;
 
   return (
-    <div
-      className={clsx(
-        "fixed top-0 w-full transition-all duration-200",
-        !lift && (bg || bgUnlift),
-        lift && `${bg || bgLift} shadow`
-      )}
-    >
-      <AppContainer className="flex h-20">{props.children}</AppContainer>
-    </div>
+    <header>
+      <div
+        className={clsx(
+          "fixed z-50 top-0 w-full transition-all duration-200",
+          !lift && (bg || bgUnlift),
+          lift && `${bg || bgLift} shadow`,
+          props.className
+        )}
+      >
+        <AppContainer className="flex h-20">{props.children}</AppContainer>
+      </div>
+      <div className="w-full h-20"></div>
+    </header>
   );
 }
 
