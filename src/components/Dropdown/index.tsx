13888bb7478b86161f9c413bd95c76082e33f2cd
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-10 13:45:02 -0300

    refactor: Make Dropdown use Button component

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -3,6 +3,7 @@ import { ChevronDownIcon } from "@heroicons/react/outline";
 import Action from "components/Action";
 import DropdownItem from "./DropdownItem";
 import clsx from "clsx";
+import Button from "components/Button";
 
 interface DropdownProps {
   label: string;
@@ -20,18 +21,19 @@ function Dropdown(props: DropdownProps) {
 
   return (
     <div className={clsx("relative inline-block", props.className)}>
-      <Action
-        className="flex items-center py-3 px-4 hover:bg-gray-50 border-2 border-black"
+      <Button
+        className="flex items-center px-4"
+        style="secondary"
         onClick={toggleDropdown}
       >
         <p className="inline-block">{props.label}</p>
         <ChevronDownIcon
           className={clsx("w-4 ml-2 transition-all", { "rotate-180": open })}
         />
-      </Action>
+      </Button>
       <ul
         className={clsx(
-          "absolute z-50 right-0 w-48 text-right shadow-md mt-2",
+          "absolute z-50 right-0 w-48 text-right shadow-md mt-2 bg-white",
           {
             hidden: !open,
           }
