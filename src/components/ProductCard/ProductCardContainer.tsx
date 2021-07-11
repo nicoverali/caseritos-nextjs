f9c2606ec6e11e352c429577e4b3e56353bfe68b
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-11 14:47:42 -0300

    fix: ProductCard focus state

--- src/components/ProductCard/ProductCardContainer.tsx
+++ src/components/ProductCard/ProductCardContainer.tsx
@@ -10,7 +10,7 @@ export default function ProductCardContaine(props: ContainerProps) {
   return (
     <div
       className={clsx(
-        "flex flex-col ring-black outline-none rounded-none focus:ring hover:ring group transition-all duration-200 ease-bounce-in",
+        "flex flex-col ring-black outline-none rounded-none group-focus:ring hover:ring group transition-all duration-200 ease-bounce-in",
         props.classNames
       )}
     >
