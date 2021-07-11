0ec44088b53d94ab1b9be5aa620e9ab00bc9c4f2
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-11 16:58:58 -0300

    feat: Add functionality to BecomeSeller

--- src/sections/Home/BecomeSeller/index.tsx
+++ src/sections/Home/BecomeSeller/index.tsx
@@ -1,14 +1,38 @@
 import TextInput from "components/TextInput";
 import UnderlineButton from "components/UnderlinedButton";
-import React from "react";
+import React, { useState } from "react";
 import SellersSVG from "components/SellersSVG";
 import clsx from "clsx";
+import MessageBox from "components/MessageBox";
+import Message from "components/MessageBox/Message";
+import { useForm } from "react-hook-form";
 
+const emailRule = {
+  required: {
+    value: true,
+    message: "Por favor completa este campo",
+  },
+  pattern: {
+    value: /^\S+@\S+$/i,
+    message: "Email invalido",
+  },
+};
 interface BecomeSellerProps {
   className?: string;
 }
 
 export default function BecomeSeller({ className }: BecomeSellerProps) {
+  const [submitted, setSubmitted] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = () => {
+    setSubmitted(true);
+    setTimeout(() => setSubmitted(false), 10000);
+  };
+
   return (
     <div
       className={clsx(
@@ -18,7 +42,7 @@ export default function BecomeSeller({ className }: BecomeSellerProps) {
     >
       <SellersSVG className="max-w-sm md:max-w-md 2xl:max-w-2xl mb-8 md:mr-8 md:mb-0" />
 
-      <div>
+      <div className="relative">
         <h2 className="text-2xl font-medium max-w-lg">
           ¿Te gustaría vender tus comidas a miles de personas?
         </h2>
@@ -27,17 +51,26 @@ export default function BecomeSeller({ className }: BecomeSellerProps) {
           Dejanos tu email para poder contactarte. Lorem ipsum dolor sit amet.
         </p>
 
-        <div className="flex flex-col items-end sm:flex-row mt-12">
+        <form className="flex flex-col items-start sm:flex-row mt-12">
           <TextInput
+            {...register("seller-email", emailRule)}
+            error={errors["seller-email"]?.message}
             className="mb-4 sm:mb-0 sm:mr-6"
             width="w-full sm:w-96"
-            name="seller-email"
             placeholder="Tu email"
             label="seller-email"
             hideLabel
           />
-          <UnderlineButton style="secondary">Enviar</UnderlineButton>
-        </div>
+          <UnderlineButton style="secondary" onClick={handleSubmit(onSubmit)}>
+            Enviar
+          </UnderlineButton>
+        </form>
+
+        <MessageBox style="success" className="absolute -bottom-14 w-full">
+          <Message show={submitted}>
+            ¡Listo! Pronto nos comunicaremos con vos.
+          </Message>
+        </MessageBox>
       </div>
     </div>
   );
