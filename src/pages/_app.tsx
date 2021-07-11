7b09b496f81c85fccb9bfccdf25917bbe89eec16
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-11 17:53:50 -0300

    feat: Add website title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import "styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
 import ClientSessionProvider from "context/ClientSessionProvider";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ClientSessionProvider>
-      <Component {...pageProps} />
-    </ClientSessionProvider>
+    <>
+      <Head>
+        <title>Caseritos</title>
+      </Head>
+      <ClientSessionProvider>
+        <Component {...pageProps} />
+      </ClientSessionProvider>
+    </>
   );
 }
 export default MyApp;
