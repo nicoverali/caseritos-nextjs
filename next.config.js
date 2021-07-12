22269cd4a057052922fa03f524c9df648ad89fe5
Nicolas Alejandro Vera <nicolasalevera98@gmail.com>
2021-07-12 00:41:55 -0300

    feat: Add next-pwa and PWA functionality

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const withPWA = require("next-pwa");
 const API_IMAGES_DOMAIN = process.env.API_IMAGES_DOMAIN;
 
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: [API_IMAGES_DOMAIN],
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+  },
+});
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,9 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
+          <meta name="theme-color" content="#F87171" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
